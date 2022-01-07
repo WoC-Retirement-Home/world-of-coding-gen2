e0d9b067a26314917be9ba0b623ce726535fbe57
Glitch (wo---rld-of-coding) <none>
2022-01-07 20:00:59 +0000

    woah!

---
+++ script.js
@@ -0,0 +1,17 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+// prints "hi" in the browser's dev tools console
+
+let helpButton=document.querySelector('#help');
+console.log('helpButton');
+let stat=document.querySelector('#livechat');
+console.log(stat);
+let counter1 = 0;
+
+helpButton.addEventListener('click', (e) => {
+   console.log('BUTTON WORKS');
+  console.log(helpButton);
+
+})
+  
+
